25e0d42296173183f203fa80855a4ab4502879b6
Agim Emruli <agimemruli@gmail.com>
2016-03-22 14:42:51 +0100

    Ribbon-config with custom rule

--- services/price-calculator/src/main/java/org/springframework/cloud/service/PriceCalculator.java
+++ services/price-calculator/src/main/java/org/springframework/cloud/service/PriceCalculator.java
@@ -1,7 +1,12 @@
 package org.springframework.cloud.service;
 
+import com.netflix.loadbalancer.IPing;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.PingUrl;
+import com.netflix.loadbalancer.WeightedResponseTimeRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author Agim Emruli
@@ -11,5 +16,15 @@ public class PriceCalculator {
 
     public static void main(String[] args) {
         new SpringApplication(PriceCalculator.class).run(args);
+    }
+
+    @Bean
+    public IPing ribbonPing() {
+        return new PingUrl(false, "/exchange/CHF/1");
+    }
+
+    @Bean
+    public IRule ribbonRule(){
+        return new WeightedResponseTimeRule();
     }
 }
